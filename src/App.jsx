b3feb1faef4d4d0631422de3a52b5e30a128cb2b
Husniddin <122876021+iamhusniddin@users.noreply.github.com>
2024-06-23 17:15:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2 md:w-1/2">
+        <Login />
+      </div>
+
+      <div className="hidden relative lg:flex items-center justify-center h-full w-1/2 bg-gray-200 md:flex">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-400 rounded-full animate-bounce " />
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
